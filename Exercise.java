c4580585b5b806a98622ab95ebdf6bad79739dfb
SIMRAN48375 <159098029+SIMRAN48375@users.noreply.github.com>
2024-02-07 10:07:32 -0500

    Add files via upload

---
+++ Exercise.java
@@ -0,0 +1,23 @@
+package javacourse;
+
+public class Exercise {
+
+	public static void main(String[] args) {
+		int marks = 78;
+		if (marks >=90) {
+			System.out.println("A");
+		}
+		else if(marks >=80) {
+			System.out.println("B");
+		}
+		else if (marks <=80) {
+			System.out.println("C");
+		}
+		else if (marks >=78) {
+			System.out.println("D");
+		}
+			
+		}
+
+	}
+
