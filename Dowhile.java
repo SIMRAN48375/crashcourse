9cecce4c03ca4d43e6ec6d2919e13eda63376ebe
SIMRAN48375 <159098029+SIMRAN48375@users.noreply.github.com>
2024-02-08 10:04:15 -0500

    Add files via upload

---
+++ Dowhile.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Dowhile {
+
+	public static void main(String[] args) {
+		int a= 1;
+		do {
+			System.out.println("Hello World");
+		} while(a>1);
+		
+		
+		
+
+	}
+
+}
