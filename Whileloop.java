bb025f4d21fd2b5283db2a243ec699e3454aa935
SIMRAN48375 <159098029+SIMRAN48375@users.noreply.github.com>
2024-02-07 20:55:41 -0500

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		
+		int a = 0;
+		
+		while(a<100) {
+			System.out.println(a);
+			a++;
+		}
+		System.out.println("Loop Finished");
+
+	}
+
+}
