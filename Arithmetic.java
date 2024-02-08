fe9f02f25de4e821b1d4b7383e70854cb29f7c87
SIMRAN48375 <159098029+SIMRAN48375@users.noreply.github.com>
2024-02-07 19:01:45 -0500

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,14 @@
+package crashcourse;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 3;
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+	}
+
+}
